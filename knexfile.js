0cd9072f5c724649feed674f23793cd609b19645
Carl Corsini <carlcorsini@Carls-MacBook-Air.local>
2018-04-17 20:40:43 -0700

    getting database on heroksies

--- knexfile.js
+++ knexfile.js
@@ -30,7 +30,7 @@ module.exports = {
   production: {
     client: 'pg',
     connection:
-      'postgres://bcydhneacicnse:69ccce18d8df7df3f71e8dd3c25b77144db173d3cd3e78ae8d9c7602f687f8a3@ec2-54-221-192-231.compute-1.amazonaws.com:5432/d5se2nbl8lp6me',
+      'postgres://qqzestzvdyvtty:2af3a5296dd198166dcfe914cfe4ca6bdc8e7dac57e14428dedcd337fa39fa01@ec2-54-221-192-231.compute-1.amazonaws.com:5432/d58bknpt0bk8fs',
     migrations: {
       directory: './db/migrations'
     },
